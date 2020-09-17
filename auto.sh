d2e0c46f49ccc4029f929680eb92685906433ae2
IoTKETI <iot.keti@gmail.com>
2020-09-17 10:43:35 +0100

    - update code for autostart

--- auto.sh
+++ auto.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/sh
 
-sudo chmod 777 /home/ETRI/node-mqtt-client-ncube-1.0.0
-npm run start
-sleep 5
+pm2 start /home/pi/ETRI/node-mqtt-client-ncube-1.0.0/build/index.js
+sleep 10
 sudo chmod 777 /home/pi/nCube-MUV/
 cd /home/pi/nCube-MUV
 sudo chmod 777 *
