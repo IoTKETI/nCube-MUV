c70cc92fdcee1d8646ad25b039cb19e5e9723c26
IoTKETI <iot.keti@gmail.com>
2020-09-17 10:06:10 +0100

    - renew code for auto start

--- auto.sh
+++ auto.sh
@@ -1,29 +1,10 @@
 #!/usr/bin/sh
 
-lte_flag=0
-ttyUSB=`ls /dev/ttyUSB3`
-while [ $lte_flag -eq 0 ]
-do
-  if [ -n "$ttyUSB" ]; then
-    echo "ttyUSB... OK ! "
-    if ping -q -c 1 -W 1 8.8.8.8 > /dev/null; then
-      lte_flag=1
-      echo "LTE... OK ! "
-    fi
-  else
-    echo "IPv4 is Down"
-    sleep 5
-    sudo reboot
-  fi
-done
-
-echo "IPv4 is Up"
-sudo chmod 777 /home/pi/nCube-sparrow/
-cd /home/pi/nCube-sparrow
-sudo chmod 777 *
-git stash
-git pull
+sudo chmod 777 /home/ETRI/node-mqtt-client-ncube-1.0.0
+npm run start
 sleep 5
-git stash pop
+sudo chmod 777 /home/pi/nCube-MUV/
+cd /home/pi/nCube-MUV
+sudo chmod 777 *
 pm2 start thyme.js
 
