43c08996039e4165efc4756961e3b9be690c5f13
IoTKETI <iot.keti@gmail.com>
2020-11-09 13:22:23 +0000

    - edit code for first start

--- auto.sh
+++ auto.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/sh
 
-pm2 start /home/pi/ETRI/node-mqtt-client-ncube-1.0.0/build/index.js
-sleep 10
+wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+sudo apt-key add mosquitto-repo.gpg.key
+cd /etc/apt/sources.list.d/
+sudo wget http://repo.mosquitto.org/debian/mosquitto-buster.list 
+sudo apt-get update
+sudo apt-get install -y mosquitto
+
+pip3 install paho-mqtt
+
 sudo chmod 777 /home/pi/nCube-MUV/
 cd /home/pi/nCube-MUV
 sudo chmod 777 *
