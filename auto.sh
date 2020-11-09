a654d42b6fcd3222b75a571ea7ba3883bb86a3ce
IoTKETI <iot.keti@gmail.com>
2020-11-09 14:26:32 +0000

    - edit code for test

--- auto.sh
+++ auto.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/sh
 
-wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
-sudo apt-key add mosquitto-repo.gpg.key
-cd /etc/apt/sources.list.d/
-sudo wget http://repo.mosquitto.org/debian/mosquitto-buster.list 
-sudo apt-get update
-sudo apt-get install -y mosquitto
-
-pip3 install paho-mqtt
-
 sudo chmod 777 /home/pi/nCube-MUV/
 cd /home/pi/nCube-MUV
 sudo chmod 777 *
