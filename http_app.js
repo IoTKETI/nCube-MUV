e4bcbf2a6e8cf560fab82c4e6ff069fcfccbd2b0
IoTKETI <iot.keti@gmail.com>
2021-07-23 13:57:30 +0900

    - update topic

--- http_app.js
+++ http_app.js
@@ -552,7 +552,7 @@ function retrieve_my_cnt_name(callback) {
 
                                     if (drone_info.hasOwnProperty('rc')) {
                                         my_rc_name = drone_info.rc;
-                                        if (mission_name === 'msw_remote_gimbal') {
+                                        if (mission_name === 'msw_lte_rc') {
                                             sub_rc_topic = '/Mobius/' + drone_info.gcs + '/RC_Data/' + my_rc_name + '/command';
                                             pub_rc_topic = '/Mobius/' + drone_info.gcs + '/Mission_Data/' + drone_info.drone + '/' + mission_name + '/' + container_name;
                                         }
