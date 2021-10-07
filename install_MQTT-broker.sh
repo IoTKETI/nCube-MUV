42b2e7163df86ffdf52092dffe4f12ec59e4fe12
dnjstjr93 <dnjstjr93@gmail.com>
2021-10-07 05:32:44 +0100

    - add file for install mqtt broker

---
+++ install_MQTT-broker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+sudo apt-key add mosquitto-repo.gpg.key
+cd /etc/apt/sources.list.d/
+sudo wget http://repo.mosquitto.org/debian/mosquitto-buster.list 
+sudo apt-get update
+sudo apt-get install -y mosquitto
