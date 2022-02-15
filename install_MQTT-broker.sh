221f8e1a95459c54f04ce47141c93b4997f463b9
IoTKETI <iot.keti@gmail.com>
2022-02-15 15:02:18 +0900

    Update install_MQTT-broker.sh

--- install_MQTT-broker.sh
+++ install_MQTT-broker.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/sh
 
-wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
-sudo apt-key add mosquitto-repo.gpg.key
-cd /etc/apt/sources.list.d/
-sudo wget http://repo.mosquitto.org/debian/mosquitto-buster.list 
-sudo apt-get update
-sudo apt-get install -y mosquitto
+OS = $(cat /etc/*release* | grep -w PRETTY_NAME | cut -d '(' -f 2 | cut -d ')' -f 1)
+
+if [ $OS = "bullseye" ];then
+  wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+  sudo apt-key add mosquitto-repo.gpg.key
+  cd /etc/apt/sources.list.d/
+  sudo wget http://repo.mosquitto.org/debian/mosquitto-bullseye.list 
+  sudo apt-get update
+  sudo apt-get install -y mosquitto
+else
+  wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+  sudo apt-key add mosquitto-repo.gpg.key
+  cd /etc/apt/sources.list.d/
+  sudo wget http://repo.mosquitto.org/debian/mosquitto-buster.list 
+  sudo apt-get update
+  sudo apt-get install -y mosquitto
+fi
