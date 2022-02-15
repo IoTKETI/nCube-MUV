012eebb0047d35302a2ed507cc2c71f052d915b2
IoTKETI <iot.keti@gmail.com>
2022-02-15 18:19:39 +0900

    Update http_app.js

--- http_app.js
+++ http_app.js
@@ -598,7 +598,7 @@ function retrieve_my_cnt_name(callback) {
                     return;
                 }
                 if (stdout === "bionic\n") {  // KEA
-                    exec("sudo gpasswd --add ${USER} dialout"), (error, stdout, stderr) => {
+                    exec("sudo chmod 777 /dev/ttyTHS0"), (error, stdout, stderr) => {
                         if (error) {  // Windows
                             console.error(`exec error: ${error}`);
                             return;
