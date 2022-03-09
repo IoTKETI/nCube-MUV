c10a2fb1d72ec285238e257787d505b4c60e86f7
dnjstjr93 <dnjstjr93@gmail.com>
2022-03-10 02:46:39 +0900

    MQTT broker 설치 명령어 수정

--- install_MQTT-broker.sh
+++ install_MQTT-broker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-OS=$(cat /etc/*release* | grep -w VERSION_CODENAME)
+OS=$(cat /etc/*release* | grep -w VERSION_CODENAME | cut -d '=' -f 2)
 
 if [ $OS = "bullseye" ];then
   wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
